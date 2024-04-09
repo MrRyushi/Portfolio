bc9067f081e5c4fffa6e0118f0ac8e606c36ec6f
MrRyushi <jptmarcellana@gmail.com>
2024-04-09 21:23:41 +0800

    second commit

---
+++ Portfolio/src/pages/Landing.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '.././index.css'
+
+export default function Landing() {
+  return (
+    <div className='flex justify-center items-center'>
+      <h1 className='text-sm text-blue-900'>Hello World!</h1>
+    </div>
+  )
+}
